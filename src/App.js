a73a6ab045ba893f3d61fdc3d7e922a0fddad63c
Payhemfoh <Payhemfoh@gmail.com>
2023-09-29 22:50:28 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,67 @@
-import logo from './logo.svg';
+import React from 'react';
+import NavBar from './Components/NavBar/navbar';
+import UserManagement from './Components/User/user';
+import ResetPassword from './Components/User/resetpassword';
+import DeleteUser from './Components/User/deleteuser';
+import AddUser from './Components/User/adduser';
+import EditUser from './Components/User/edituser';
+import Home from './Components/Home/home'
+import Login from './Components/Login/login';
+import useToken from './Components/Token/token'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Container, Button, darkColors, lightColors } from 'react-floating-action-button'
 import './App.css';
 
-function App() {
+const FABStyle = {
+  width: 100 + "%",
+  height: 75 + "px",
+  borderRadius: 15 + "px",
+  backgroundColor: darkColors.lighterRed,
+  color: lightColors.white,
+  fontSize: 25 + "px"
+}
+
+const App = () => {
+  const { setToken, clearToken, token, role } = useToken();
+
+  const LogOut = () => {
+    clearToken();
+    window.location = "/";
+  }
+
+  const LogIn = token => {
+    setToken(token);
+    window.location = "/";
+  }
+
+  const LogOutFAB = () => {
+    if (!token) return (<div></div>);
+
+    return (
+      <Container className="LogOutFAB">
+        <Button styles={FABStyle} onClick={LogOut}> LogOut </Button>
+      </Container >
+    )
+  }
+
+  if (!token) {
+    return <Login setToken={LogIn} />;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <NavBar role={role} />
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/user" element={<UserManagement />} />
+        <Route path="/user/adduser" element={<AddUser />} />
+        <Route path="/user/edituser" element={<EditUser />} />
+        <Route path="/user/deleteuser" element={<DeleteUser />} />
+        <Route path="/user/resetpassword" element={<ResetPassword />} />
+        <Route path="/logout" Component={LogOut} />
+      </Routes>
+      <LogOutFAB />
+    </Router>
   );
 }
 
---
+++ src/Components/Home/home.js
@@ -0,0 +1,14 @@
+import '../../App.css';
+
+export default function Home() {
+    return (
+        <div className="App">
+            <button>
+                Start Order
+            </button>
+            <button>
+                Edit Menu
+            </button>
+        </div>
+    );
+}
