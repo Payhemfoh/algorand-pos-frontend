d82ac9e74a457e5748c6698ba68665f0d86b7d26
Tiffany <tiffprag@gmail.com>
2023-10-02 19:22:41 +0800

    Added delete voucher feature

--- src/Components/Voucher/deletevoucher.js
+++ src/Components/Voucher/deletevoucher.js
@@ -1,53 +1,81 @@
 import React, { useState } from 'react';
 import '../User/user.css';
 
-async function deleteUser(credentials) {
-    return fetch('http://localhost:5000/deleteuser', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(credentials)
-    })
-        .then(data => { window.alert("Success"); window.location = "/user"; })
-        .catch(e => window.alert("failed"))
+async function deleteVoucher(input) {
+    return fetch('http://localhost:5002/delete_redeem/' +  input.name)
+        .then(async (res) => { 
+            return await res.json()
+        })
+        .catch(e => {
+            return "Error"
+        })
 }
 
-export default function DeleteUser() {
-    const [username, setUserName] = useState();
+export default function DeleteVoucher() {
+    const [name, setName] = useState();
+    const [result, setResult] = useState();
+    const [error, setError] = useState();
 
-    const backToUserManagement = async e => {
+    const backToVoucherManagement = async e => {
         e.preventDefault();
-        window.location = "/user";
+        window.location = "/voucher";
     }
 
     const handleSubmit = async e => {
         e.preventDefault();
-        const token = await deleteUser({
-            username
-        });
+        setError(null)
+        if (name) {
+            var response = await deleteVoucher({
+                "name": name.replace(" ", "%20")
+            });
+
+            if (response == "Error") {
+                setError("Unable to find voucher")
+            } else if (response.data.length > 0){
+                setResult(response.data);
+            }
+        }
     }
 
     return (
         <div className="login_background">
             <div className="UserFormContainer">
-                <h4>Delete User</h4>
+                <h4>Delete Voucher</h4>
                 <form>
                     <div className="text_area">
                         <input
                             type="text"
-                            id="username"
-                            name="username"
-                            placeholder="Username"
+                            id="name"
+                            name="name"
+                            placeholder="name"
                             className="text_input"
-                            onChange={e => setUserName(e.target.value)}
+                            onChange={e => setName(e.target.value)}
                         />
                     </div>
                     <button className="btn" onClick={handleSubmit}>
-                        Delete User
+                        Submit
                     </button>
-                    <button className="btn" onClick={backToUserManagement}> Back </button>
-                </form>
+                    <button className="btn" onClick={backToVoucherManagement}> Back </button>
+                    
+                    <br />
+                    <br />
+
+                    {result && 
+                    <div style={{textAlign:'left'}}>
+                        <hr />
+
+                        {error &&
+                            <p style={{color:'red'}}>{error}</p>
+                        }
+
+                        {!error &&
+                            <>
+                                <p>Voucher deleted!</p>
+                            </>
+                        }
+                    </div>
+                    }
+                    </form>
             </div>
         </div>
     )
